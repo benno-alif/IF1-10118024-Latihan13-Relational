15e133e30551a13eceae5977618581f6f61b3a60
BennoAlif <bennoalif41@gmail.com>
2019-09-26 12:10:47 +0700

    Initial commit

---
+++ src/com/benno/Main.java
@@ -0,0 +1,22 @@
+package com.benno;
+
+    /*
+    NAMA        : BENNO ALIF ANGGARA
+    KELAS       : IF1
+    NIM         : 10118024
+    Deskripsi   : Implementasi Relational
+     */
+
+public class Main {
+
+    public static void main(String[] args) {
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 <= bil1));
+    }
+}
